5b9635e2889579ac79becdab529d3507dc412a9b
Bstz-att1 <hfdarioalexander7@gmail.com>
2026-04-12 22:04:17 -0500

    feat: implementa archivo barril index.js para centralizar exportaciones de módulos

---
+++ assets/js/index.js
@@ -0,0 +1,4 @@
+export { renderInventarioSection } from "./inventario.js";
+export { renderMovimientosSection } from "./movimientos.js";
+export { renderAuditoriaSection } from "./auditoria.js";
+export { renderAdministracionSection } from "./administracion.js";
