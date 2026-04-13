fac4c962b5880f6325fbf30d6d850b1bf62205a1
Bstz-att1 <hfdarioalexander7@gmail.com>
2026-04-13 00:26:08 -0500

    refactor: amplía archivo barril para exponer funciones de usuarios y sesión

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,3 +2,9 @@ export { renderInventarioSection } from "./inventario.js";
 export { renderMovimientosSection } from "./movimientos.js";
 export { renderAuditoriaSection } from "./auditoria.js";
 export { renderAdministracionSection } from "./administracion.js";
+export {
+  renderUsuariosSection,
+  renderLoginSection,
+  getCurrentSessionUser,
+  logoutSessionUser,
+} from "./usuarios.js";
